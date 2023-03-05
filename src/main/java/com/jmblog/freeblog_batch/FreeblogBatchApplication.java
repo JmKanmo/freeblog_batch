0c3826739f5c9275c3366f8270e31766047234a5
jmkanmo <nebi25@naver.com>
2023-03-05 23:44:47 +0900

    first commit

---
+++ src/main/java/com/jmblog/freeblog_batch/FreeblogBatchApplication.java
@@ -0,0 +1,13 @@
+package com.jmblog.freeblog_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeblogBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeblogBatchApplication.class, args);
+    }
+
+}
